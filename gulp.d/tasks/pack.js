3299e97bf9c1d604c2c526fa7be0135deca18e90
Martin Mattel <github@diemattels.at>
2026-01-15 14:35:53 +0100

    intendation fix

--- gulp.d/tasks/pack.js
+++ gulp.d/tasks/pack.js
@@ -12,8 +12,8 @@ const zip = (() => {
 
 module.exports = (src, dest, bundleName, onFinish) => () =>
   vfs
-     // encoding: false == avoid any transcoding, and effectively just pass around raw binary data.
-     // if omitted, image files such as .png will be transcoded and become useless
+  // encoding: false == avoid any transcoding, and effectively just pass around raw binary data.
+  // if omitted, image files such as .png will be transcoded and become useless
     .src('**/*', { base: src, cwd: src, dot: true, encoding: false })
     .pipe(zip.dest(ospath.join(dest, `${bundleName}-bundle.zip`)))
     .on('finish', () => onFinish && onFinish(ospath.resolve(dest, `${bundleName}-bundle.zip`)))
